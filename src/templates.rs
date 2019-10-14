8b76329491cde05769be6a0a97fd62a4b242927e
Louis de Wardt <contact@louis.dewardt.uk>
2019-10-14 19:34:17 +0100

    Courses are parsed then rendered on a web server (basic)

---
+++ src/templates.rs
@@ -0,0 +1,32 @@
+use askama::Template;
+use serde::{Deserialize, Deserializer, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Template, Clone)]
+#[template(path = "course.html", escape = "none")]
+pub struct Course {
+    pub lang: String,
+    pub title: String,
+    pub tutorials: Vec<Tutorial>,
+}
+
+fn apply_markdown<'de, D>(deserializer: D) -> Result<String, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    use pulldown_cmark::{html, Parser};
+
+    let content = String::deserialize(deserializer)?;
+    let parser = Parser::new(&content);
+
+    let mut html_buf = String::new();
+    html::push_html(&mut html_buf, parser);
+
+    Ok(html_buf)
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Tutorial {
+    pub subtitle: String,
+    #[serde(deserialize_with = "apply_markdown")]
+    pub content: String,
+}
